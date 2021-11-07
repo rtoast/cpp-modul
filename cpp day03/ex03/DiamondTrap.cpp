c794147bc5d1a8f9dc5788b8b17891cdd1e767c3
Reelydoit Toast <rtoast@un-a5.kzn.21-school.ru>
2021-11-07 08:05:52 +0300

    fuck

--- cpp day03/ex03/DiamondTrap.cpp
+++ cpp day03/ex03/DiamondTrap.cpp
@@ -1,18 +1,18 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(): ClapTrap(){
-    DiamondTrap::_Hitpoints = FragTrap::_Hitpoints;
-    DiamondTrap::_EnergyPoints = ScavTrap::_EnergyPoints;
-    DiamondTrap::_AttakDamage = FragTrap::_AttakDamage;
+DiamondTrap::DiamondTrap(): ClapTrap(), ScavTrap(), FragTrap(){
+    _Hitpoints = FragTrap::_Hitpoints;
+    _EnergyPoints = ScavTrap::_EnergyPoints;
+    _AttakDamage = FragTrap::_AttakDamage;
     std::cout << "Default constructor DiamondTrap called" << std::endl;
 }
 
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"){
     ClapTrap::_Name = name + "_clap_name";
-    DiamondTrap::name = name;
-    DiamondTrap::_Hitpoints = FragTrap::_Hitpoints;
-    DiamondTrap::_EnergyPoints = ScavTrap::_EnergyPoints;
-    DiamondTrap::_AttakDamage = FragTrap::_AttakDamage;
+    _name = name;
+    _Hitpoints = FragTrap::_Hitpoints;
+    _EnergyPoints = ScavTrap::_EnergyPoints;
+    _AttakDamage = FragTrap::_AttakDamage;
     std::cout << "Name constructor DiamondTrap called" << std::endl;
 }
 
@@ -29,7 +29,7 @@ DiamondTrap &DiamondTrap::operator=(const DiamondTrap &equals){
     std::cout << "Assignation FragTrap operator called" << std::endl;
 	if (this == &equals)
 		return (*this);
-	name = equals.name;
+	_name = equals._name;
 	_Hitpoints = equals._Hitpoints;
 	_EnergyPoints = equals._EnergyPoints;
 	_AttakDamage = equals._AttakDamage;
@@ -37,7 +37,7 @@ DiamondTrap &DiamondTrap::operator=(const DiamondTrap &equals){
 }
 
 void DiamondTrap::whoAmI(){
-    std::cout << "DiamondTrap name: " << name << std::endl;
+    std::cout << "DiamondTrap name: " << _name << std::endl;
     std::cout << "ClapTrap name: " << ClapTrap::_Name << std::endl;
 }
 
--- cpp day03/ex03/DiamondTrap.hpp
+++ cpp day03/ex03/DiamondTrap.hpp
@@ -8,7 +8,7 @@
 
 class DiamondTrap : public ScavTrap, public FragTrap{
     private:
-        std::string name;
+        std::string _name;
     protected:
 		unsigned int _Hitpoints;
 		unsigned int _EnergyPoints;
