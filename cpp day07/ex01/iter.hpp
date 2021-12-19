c826ee9105a60687029e8c2f403cba3334d25387
Reelydoit Toast <rtoast@si-h4.kzn.21-school.ru>
2021-12-19 07:46:09 +0300

    ex01

--- cpp day07/ex01/iter.hpp
+++ cpp day07/ex01/iter.hpp
@@ -1,4 +1,17 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
 template <class T>
-T funk () {
-  
+void OMG (T symbol) {
+    std::cout << "OMG it's " << symbol << std::endl;
 }
+
+template <class T, class U>
+void iter (T *mas, U length, void (*func)(T const &t)) {
+    for (U i = 0; i < length; i++)
+		func(mas[i]);
+}
+
+#endif
---
+++ cpp day07/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "iter.hpp"
+
+int		main() {
+	int arr[3] = {1, 2, 3}; 
+	iter(arr, 3, OMG);
+
+    double Darr[3] = {4.7, 5.8, 6.9}; 
+	iter(Darr, 3, OMG);
+
+    char Carr[3] = {'O', 'M', 'G'}; 
+	iter(Carr, 3, OMG);
+
+    return(0);
+}
